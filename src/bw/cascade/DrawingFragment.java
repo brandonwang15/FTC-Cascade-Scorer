83dac47f4ff794cb8c72b6938019e59e7d8acbee
Brandon Wang <315bwang@gmail.com>
2014-12-06 21:45:27 -0500

    Got saving of route images to work
    next need to display notes and add to the database thingy

--- src/bw/cascade/DrawingFragment.java
+++ src/bw/cascade/DrawingFragment.java
@@ -3,10 +3,11 @@ package bw.cascade;
 
 import java.util.ArrayList;
 
-import bw.cascade.misc.BitmapEntry;
-
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -18,9 +19,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import bw.cascade.misc.BitmapEntry;
 
 public class DrawingFragment extends Fragment implements OnClickListener{
 	
@@ -132,12 +135,68 @@ public class DrawingFragment extends Fragment implements OnClickListener{
 	 */
 	private void saveCurrentRoute()
 	{
-		BitmapEntry entry = new BitmapEntry();
-		//TODO: prompt user to input title 
-		entry.setBitmapName("placeholder_name");
-		entry.setBitmap(getAutonomousBitmap());
-		callback.addSavedAutoRoute(entry);
-		Toast.makeText(getActivity(), "Image saved!", Toast.LENGTH_LONG).show();
+		final BitmapEntry entry = new BitmapEntry();
+		
+		//Create dialog to prompt for save name and notes
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+		View dialogView = inflater.inflate(R.layout.dialog_text_input, null);
+		final EditText nameField = (EditText) dialogView.findViewById(R.id.nameField);
+		final EditText notesField = (EditText)dialogView.findViewById(R.id.extraInfoField);
+		
+		//initalize dialog
+		builder.setView(dialogView)
+				.setPositiveButton(R.string.save_bitmap, new DialogInterface.OnClickListener(){
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						return;
+						//nothing for now, will be overriden later
+					}		
+				})
+				.setNegativeButton(R.string.cancel_bitmap, new DialogInterface.OnClickListener(){
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						return;//do nothing
+					}			
+				})
+				.setTitle("Save Route");
+		final AlertDialog dialog = builder.create();
+		dialog.show();
+		
+		//override the dialog button behavior here, so that the automatic closing of the dialog when a button is clicked can be avoided
+		dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String name = nameField.getText().toString().trim();
+				
+				//name must have non-whitespace characters
+				if(name.equals(""))
+				{
+					Toast.makeText(getActivity(), "You can't input an empty name!", Toast.LENGTH_LONG).show();
+					return;//end without closing dialog
+				}
+				
+				//check if given name already exists
+				if(callback.doesSaveNameExist(name))
+				{
+					Toast.makeText(getActivity(), "A saved route with the name:\"" +nameField.getText().toString() +"\" already exists!", Toast.LENGTH_LONG).show();
+					return;//end without closing dialog
+				}
+				
+				//if all data is valid
+				entry.setName(name);
+				entry.setBitmap(getAutonomousBitmap());
+				entry.setNotes(notesField.getText().toString().trim());
+				callback.addSavedAutoRoute(entry);
+				Toast.makeText(getActivity(), "Image saved!", Toast.LENGTH_LONG).show();
+				dialog.dismiss();
+				
+			}
+		});
+	
 	}
 	
 	/***
--- src/bw/cascade/SavedRoutesFragment.java
+++ src/bw/cascade/SavedRoutesFragment.java
@@ -7,12 +7,15 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import bw.cascade.misc.BitmapEntry;
 
@@ -21,9 +24,14 @@ public class SavedRoutesFragment extends Fragment {
 	private FrameLayout drawingViewFrame;
 	private DrawingView drawingView;
 	private Spinner chooseRouteSpinner;
+	private Button deleteEntryBtn;
+	private TextView notesView;
+	
 	private ArrayAdapter<String> spinnerData;
 	private ArrayList<BitmapEntry> savedRoutes;
+	private ArrayList<String> adapterData;//choices for array adapter
 	
+	private MainActivity callback;
 	public SavedRoutesFragment(ArrayList<BitmapEntry> savedRoutes)
 	{
 		this.savedRoutes = savedRoutes;
@@ -33,7 +41,7 @@ public class SavedRoutesFragment extends Fragment {
 	public void onCreate(Bundle savedInstanceState){
 		super.onActivityCreated(savedInstanceState);
 		
-		
+		callback = (MainActivity)getActivity();
 	}
 	
 	
@@ -42,8 +50,19 @@ public class SavedRoutesFragment extends Fragment {
 	{
 		View v = inflater.inflate(R.layout.fragment_saved_routes, container,false);
 		
+		notesView = (TextView)v.findViewById(R.id.notesView);
 		drawingViewFrame = (FrameLayout) v.findViewById(R.id.savedRouteDisplayFrame);
 		chooseRouteSpinner = (Spinner)v.findViewById(R.id.selectSavedImageSpinner);
+		deleteEntryBtn = (Button)v.findViewById(R.id.deleteSaveBtn);
+		deleteEntryBtn.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v) {
+				deleteCurrentEntry();
+			}
+			
+		});
 		
 		initializeDrawingView();
 		populateSpinner();
@@ -52,15 +71,44 @@ public class SavedRoutesFragment extends Fragment {
 	}
 
 	/**
+	 * Deletes the current route
+	 */
+	private void deleteCurrentEntry()
+	{
+		int pos = chooseRouteSpinner.getSelectedItemPosition();
+		
+		if(pos == -1)
+		{
+			Toast.makeText(getActivity(), "Nothing to delete!", Toast.LENGTH_LONG).show();
+			return;
+		}
+		
+		BitmapEntry entryToDelete = savedRoutes.get(pos);
+		callback.deleteSavedAutoRoute(entryToDelete);
+		
+		//update which route is now displayed
+		notifyRouteDataChanged();
+		if(pos >= savedRoutes.size())//if the last route in the list was deleted, display the new last route
+		{
+			pos = savedRoutes.size()-1;
+		}
+		if(savedRoutes.size() == 0)//if the list is now empty
+		{
+			drawingView.setVisibility(View.INVISIBLE);
+			notesView.setVisibility(View.INVISIBLE);
+		}
+	}
+	
+	/**
 	 * populate the spinner with list of saved routes that can be displayed
 	 */
 	private void populateSpinner()
 	{
-		ArrayList<String> choices = new ArrayList<String>();
+		adapterData = new ArrayList<String>();
 		for(BitmapEntry b : savedRoutes)
-			choices.add(b.getBitmapName());
+			adapterData.add(b.getName());
 		
-		spinnerData = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, android.R.id.text1,choices);
+		spinnerData = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, android.R.id.text1,adapterData);
 		chooseRouteSpinner.setAdapter(spinnerData);
 		chooseRouteSpinner.setOnItemSelectedListener(new OnItemSelectedListener(){
 
@@ -68,6 +116,9 @@ public class SavedRoutesFragment extends Fragment {
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int position, long id) {
 				displayBitmapEntry(savedRoutes.get(position));
+				notesView.setText(savedRoutes.get(position).getNotes());
+				drawingView.setVisibility(View.VISIBLE);
+				notesView.setVisibility(View.VISIBLE);
 			}
 
 			@Override
@@ -85,7 +136,14 @@ public class SavedRoutesFragment extends Fragment {
 	public void notifyRouteDataChanged()
 	{
 		if(spinnerData != null)
+		{
+			//update choices
+			adapterData.clear();
+			for(BitmapEntry b : savedRoutes)
+				adapterData.add(b.getName());
+			
 			spinnerData.notifyDataSetChanged();
+		}
 	}
 	
 	private void initializeDrawingView()
@@ -106,7 +164,7 @@ public class SavedRoutesFragment extends Fragment {
 	{
 		Bitmap b = entry.getBitmap();
 		if(b == null)
-			Toast.makeText(getActivity(), entry.getBitmapName() +" was not able to be displayed.", Toast.LENGTH_LONG).show();
+			Toast.makeText(getActivity(), entry.getName() +" was not able to be displayed.", Toast.LENGTH_LONG).show();
 		else
 		{
 			drawingView.setNewImage(entry.getBitmap());
